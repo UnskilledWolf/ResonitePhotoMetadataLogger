18b0fca34bf41c595723461afdd5cc239a613f8e
AxiomWolf <geronimo.xl13@gmail.com>
2026-01-06 14:11:23 +0100

    Initialized based on ExampleMod Template

---
+++ PhotoMetadataLogger/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("PhotoMetadataLogger")]
+[assembly: AssemblyProduct("PhotoMetadataLogger")]
+[assembly: AssemblyDescription("Description of your mod")]
+[assembly: AssemblyCompany("AxiomWolf")]
+[assembly: AssemblyCopyright("Copyright © 2026 PhotoMetadataLogger")]
+[assembly: AssemblyVersion(PhotoMetadataLogger.PhotoMetadataLogger.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(PhotoMetadataLogger.PhotoMetadataLogger.VERSION_CONSTANT)]

891a9985fac8b1d69d4f8948da7ee898f7a4d81b
AxiomWolf <geronimo.xl13@gmail.com>
2026-01-06 16:53:40 +0100

    Initial Working Version

--- PhotoMetadataLogger/PhotoMetadataLogger.cs
+++ PhotoMetadataLogger/PhotoMetadataLogger.cs
@@ -1,6 +1,12 @@
+using Elements.Core;
+
 using FrooxEngine;
+
 using HarmonyLib;
+
 using ResoniteModLoader;
+
+using System.Text.Json;
 
 namespace PhotoMetadataLogger;
 //More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
@@ -16,11 +22,61 @@ public class PhotoMetadataLogger : ResoniteMod {
 		harmony.PatchAll();
 	}
 
-	//Example of how a HarmonyPatch can be formatted, Note that the following isn't a real patch and will not compile.
-	// [HarmonyPatch(typeof(ClassNameHere), "MethodNameHere")]
-	// class ClassNameHere_MethodNameHere_Patch {
-	// 	static void Postfix(ClassNameHere __instance) {
-	// 		Msg("Postfix from ExampleMod");
-	// 	}
-	// }
+	// Append exporting of Metadata to the PhotoMetadata's Save Screenshot context menu option
+	[HarmonyPatch(typeof(PhotoMetadata), "NotifyOfScreenshot")]
+	class PhotoMetadata_NotifyOfScreenshot_ExportMetadata {
+		static void Postfix(PhotoMetadata __instance) {
+			Msg("Exporting photo summary.");
+
+			PhotoMetadataSummary summary = new(__instance);
+			var options = new JsonSerializerOptions {
+				IncludeFields = true,
+			};
+			string jsonString = JsonSerializer.Serialize(summary, options);
+			File.AppendAllText(@"/home/axiom/Pictures/Resonite.jsonl", jsonString + Environment.NewLine);
+		}
+	}
+
+	public class PhotoMetadataSummary(PhotoMetadata photo) {
+		public readonly string CameraManufacturer = photo.CameraManufacturer.Value ?? "N/A";
+		public readonly string CameraModel = photo.CameraModel.Value ?? "N/A";
+		public readonly float CameraFOV = photo.CameraFOV.Value;
+		public readonly bool Is360 = photo.Is360.Value;
+		public readonly bool IsStereo = photo.StereoLayout.Value != StereoLayout.None;
+		public readonly string LocationName = photo.LocationName.Value ?? "Unknown";
+		public readonly string LocationURL = photo.LocationURL.Value != null ? photo.LocationURL.Value.AbsoluteUri : "";
+		public readonly UserSummary LocationHost = new(photo.LocationHost);
+		public readonly string LocationAccessLevel = photo.LocationAccessLevel.ToString();
+		public readonly bool LocationHiddenFromListing = photo.LocationHiddenFromListing.Value ?? false;
+		public readonly long TimeTaken = new DateTimeOffset(photo.TimeTaken.Value).ToUnixTimeSeconds();
+		public readonly UserSummary TakenBy = new(photo.TakenBy);
+		public readonly float[] TakenGlobalPosition = FlattenFloat3(photo.TakenGlobalPosition.Value);
+		public readonly float[] TakenGlobalRotation = FlattenFloat3(photo.TakenGlobalRotation.Value.EulerAngles);
+		public readonly float[] TakenGlobalScale = FlattenFloat3(photo.TakenGlobalScale.Value);
+		public readonly string AppVersion = photo.AppVersion.Value;
+		public readonly WorldUserSummary[] UserInfos = ProcessUserInfos(photo.UserInfos);
+	}
+
+	public static float[] FlattenFloat3(float3 input) {
+		return [input[0], input[1], input[2]];
+	}
+
+	public static WorldUserSummary[] ProcessUserInfos(SyncList<AssetMetadata.UserInfo> usersList) {
+		var items = usersList.Select(user => new WorldUserSummary(user));
+		return [.. items];
+	}
+
+	public class WorldUserSummary(AssetMetadata.UserInfo userInfo) {
+		public readonly UserSummary User = new(userInfo.User);
+		public readonly bool IsInVR = userInfo.IsInVR;
+		public readonly bool IsPresent = userInfo.IsPresent;
+		public readonly float[] HeadPosition = FlattenFloat3(userInfo.HeadPosition.Value);
+		public readonly float[] HeadOrientation = FlattenFloat3(userInfo.HeadOrientation.Value.EulerAngles);
+		public readonly float SessionJoinTimestamp = new DateTimeOffset(userInfo.SessionJoinTimestamp).ToUnixTimeSeconds();
+	}
+
+	public class UserSummary(UserRef user) {
+		public readonly string Username = user.User != null ? user.Target.UserName : "";
+		public readonly string UserId = user.LinkedCloudId;
+	}
 }
